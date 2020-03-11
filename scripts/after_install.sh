10c9e1b39fb9cb1068d35461a60de315de493aac
prakashbharti786 <prakashbharti786@gmail.com>
2020-03-11 21:03:50 +0530

    buildspec.yml and after_install.sh updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 BASE_PATH="/home/ubuntu/pmh_app/"
-mkdir ${BASE_PATH}releases/"${DEPLOYMENT_ID}"
-mv ${BASE_PATH}build/* ${BASE_PATH}releases/"${DEPLOYMENT_ID}"/
+rm -rf ${BASE_PATH}app
+mkdir ${BASE_PATH}app
+mv ${BASE_PATH}build/* ${BASE_PATH}app/
 rm -rf ${BASE_PATH}build
 pm2 delete ${BASE_PATH}ecosystem.config.js
-rm -rf ${BASE_PATH}current
-mkdir ${BASE_PATH}current
-cp ${BASE_PATH}.env ${BASE_PATH}releases/${DEPLOYMENT_ID}/
-ln -s ${BASE_PATH}releases/"${DEPLOYMENT_ID}"/* ${BASE_PATH}current/
-ln -s ${BASE_PATH}releases/"${DEPLOYMENT_ID}"/.env ${BASE_PATH}/current/
+cp ${BASE_PATH}.env ${BASE_PATH}app/
 pm2 flush # Clear all the logs
 pm2 start ${BASE_PATH}ecosystem.config.js
-ARRAY=()
-for f in "${BASE_PATH}"releases/*; do
-    if [ -d "$f" ] && [ $f != ${BASE_PATH}releases/"${DEPLOYMENT_ID}" ]; then
-        ARRAY+=("$f")
-    fi
-done
-## now loop through the above array
-for i in "${ARRAY[@]}"; do
-  rm -rf $i
-done
