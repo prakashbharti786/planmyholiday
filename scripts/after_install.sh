c5c052c2a8f10053a22cede7de69c5f0528d7a61
prakashbharti786 <prakashbharti786@gmail.com>
2020-03-11 21:22:11 +0530

    after_install.sh updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,5 +6,6 @@ mv ${BASE_PATH}build/* ${BASE_PATH}app/
 rm -rf ${BASE_PATH}build
 pm2 delete ${BASE_PATH}ecosystem.config.js
 cp ${BASE_PATH}.env ${BASE_PATH}app/
+npm install --production --prefix ${BASE_PATH}app/
 pm2 flush # Clear all the logs
 pm2 start ${BASE_PATH}ecosystem.config.js
