cd4810d53bb510c25db66dfdc2859dc50a3640ec
prakashbharti786 <prakashbharti786@gmail.com>
2020-03-11 22:50:26 +0530

    application_start.sh updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,8 +6,4 @@ mv ${BASE_PATH}build/* ${BASE_PATH}app/
 rm -rf ${BASE_PATH}build
 pm2 delete ${BASE_PATH}ecosystem.config.js
 cp ${BASE_PATH}.env ${BASE_PATH}app/
-npm install --production --prefix ${BASE_PATH}app/
-pm2 flush # Clear all the logs
-cd $BASE_PATH || exit 0
-/usr/bin/pm2 start ecosystem.config.js
-exec bash
+
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+BASE_PATH="/home/ubuntu/pmh_app/"
+cd $BASE_PATH
+npm install --production --prefix ${BASE_PATH}app/
+pm2 flush # Clear all the logs
+# shellcheck disable=SC2164
+/usr/bin/pm2 start ecosystem.config.js
+exec bash
